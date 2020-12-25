0165b7a0666f858070473fe6290bc66be2d2479b
darkpchela <darkpchela2@gmail.com>
2020-12-26 01:35:01 +0300

    Make data layer UnitOfWork

---
+++ DataAccessLayer/Interfaces/ICPUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface ICPUnitOfWork : IDisposable
+    {
+        ICollectionItemRepository CollectionItemRepository { get; }
+
+        ICollectionOptionalFieldRepository CollectionOptionalFieldRepository { get; }
+
+        ICollectionsRepository CollectionsRepository { get; }
+
+        ICommentsRepository CommentsRepository { get; }
+
+        IItemCommentRepository ItemCommentRepository { get; }
+
+        IItemLikeRepository ItemLikeRepository { get; }
+
+        IItemOptionalFieldRepository ItemOptionalFieldRepository { get; }
+
+        IItemsRepository ItemsRepository { get; }
+
+        IItemTagRepository ItemTagRepository { get; }
+
+        IOptionalFieldsRepository OptionalFieldsRepository { get; }
+
+        ITagsRepository TagsRepository { get; }
+
+        IThemesRepository ThemesRepository { get; }
+
+        IUsersRepository UsersRepository { get; }
+    }
+}
---
+++ DataAccessLayer/Services/CPUnitOfWork.cs
@@ -0,0 +1,175 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Services.Repositories;
+using System;
+
+namespace DataAccessLayer.Services
+{
+    public class CPUnitOfWork : ICPUnitOfWork
+    {
+        private readonly CPDbContext dbContext;
+
+        private ICollectionItemRepository collectionItemRepository;
+
+        private ICollectionOptionalFieldRepository collectionOptionalFieldRepository;
+
+        private ICollectionsRepository collectionsRepository;
+
+        private ICommentsRepository commentsRepository;
+
+        private IItemCommentRepository itemCommentRepository;
+
+        private IItemLikeRepository itemLikeRepository;
+
+        private IItemOptionalFieldRepository itemOptionalFieldRepository;
+
+        private IItemsRepository itemsRepository;
+
+        private IItemTagRepository itemTagRepository;
+
+        private IOptionalFieldsRepository optionalFieldsRepository;
+
+        private ITagsRepository tagsRepository;
+
+        private IThemesRepository themesRepository;
+
+        private IUsersRepository usersRepository;
+
+        public ICollectionItemRepository CollectionItemRepository
+        {
+            get
+            {
+                return collectionItemRepository ?? (collectionItemRepository = new CollectionItemRepository(dbContext));
+            }
+        }
+
+        public ICollectionOptionalFieldRepository CollectionOptionalFieldRepository
+        {
+            get
+            {
+                return collectionOptionalFieldRepository ?? (collectionOptionalFieldRepository = new CollectionOptionalFieldRepository(dbContext));
+            }
+        }
+
+        public ICollectionsRepository CollectionsRepository
+        {
+            get
+            {
+                return collectionsRepository ?? (collectionsRepository = new CollectionsRepository(dbContext));
+            }
+        }
+
+        public ICommentsRepository CommentsRepository
+        {
+            get
+            {
+                return commentsRepository ?? (commentsRepository = new CommentsRepository(dbContext));
+            }
+        }
+
+        public IItemCommentRepository ItemCommentRepository
+        {
+            get
+            {
+                return itemCommentRepository ?? (itemCommentRepository = new ItemCommentRepository(dbContext));
+            }
+        }
+
+        public IItemLikeRepository ItemLikeRepository
+        {
+            get
+            {
+                return itemLikeRepository ?? (itemLikeRepository = new ItemLikeRepository(dbContext));
+            }
+        }
+
+        public IItemOptionalFieldRepository ItemOptionalFieldRepository
+        {
+            get
+            {
+                return itemOptionalFieldRepository ?? (itemOptionalFieldRepository = new ItemOptionalFieldRepository(dbContext));
+            }
+        }
+
+        public IItemsRepository ItemsRepository
+        {
+            get
+            {
+                return itemsRepository ?? (itemsRepository = new ItemsRepository(dbContext));
+            }
+        }
+
+        public IItemTagRepository ItemTagRepository
+        {
+            get
+            {
+                return itemTagRepository ?? (itemTagRepository = new ItemTagRepository(dbContext));
+            }
+        }
+
+        public IOptionalFieldsRepository OptionalFieldsRepository
+        {
+            get
+            {
+                return optionalFieldsRepository ?? (optionalFieldsRepository = new OptionalFieldsRepository(dbContext));
+            }
+        }
+
+        public ITagsRepository TagsRepository
+        {
+            get
+            {
+                return tagsRepository ?? (tagsRepository = new TagsRepository(dbContext));
+            }
+        }
+
+        public IThemesRepository ThemesRepository
+        {
+            get
+            {
+                return themesRepository ?? (themesRepository = new ThemesRepository(dbContext));
+            }
+        }
+
+        public IUsersRepository UsersRepository
+        {
+            get
+            {
+                return usersRepository ?? (usersRepository = new UsersRepository(dbContext));
+            }
+        }
+
+        public CPUnitOfWork(CPDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        #region Disposable
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    dbContext.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        ~CPUnitOfWork()
+        {
+            Dispose(false);
+        }
+
+        #endregion Disposable
+    }
+}
