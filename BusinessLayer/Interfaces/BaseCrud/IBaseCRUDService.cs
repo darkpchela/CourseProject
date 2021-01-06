3d8aeaa7116f8bf3c144b77091ca0bebf4739f8c
darkpchela <darkpchela2@gmail.com>
2021-01-06 20:11:13 +0300

    Rename namespace

---
+++ BusinessLayer/Interfaces/BaseCrud/IBaseCRUDService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces.BaseCrud
+{
+    public interface IBaseCRUDService<TEntity, TModel> where TEntity : class where TModel : class
+    {
+        Task CreateAsync(TModel model);
+
+        Task<TModel> GetAsync(int id);
+
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task Update(TModel model);
+
+        Task DeleteAsync(int id);
+    }
+}
