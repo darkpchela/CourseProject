2b955889cf8c1e8be0b2df2f4f5e3e3219ec8ae7
darkpchela <darkpchela2@gmail.com>
2021-01-14 00:57:49 +0300

    Update Edit Item PL logic. Make mapping profiles. Make edit item base script. Fix js bug

---
+++ CourseProject/MappingProfiles/EditItemOptionalFieldProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BusinessLayer.Models.DALModels;
+using CourseProject.ViewModels;
+
+namespace CourseProject.MappingProfiles
+{
+    public class EditItemOptionalFieldProfile : Profile
+    {
+        public EditItemOptionalFieldProfile()
+        {
+            CreateMap<ItemOptionalFieldModel, EditItemOptionalFieldVM>()
+                .ForMember(d => d.FieldId, o => o.MapFrom(s => s.OptionalFieldId))
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.OptionalField.Name))
+                .ForMember(d => d.Type, o => o.MapFrom(s => s.OptionalField.Type.Type));
+        }
+    }
+}
---
+++ CourseProject/MappingProfiles/EditItemVMProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BusinessLayer.Models.DALModels;
+using CourseProject.ViewModels;
+using System.Linq;
+
+namespace CourseProject.MappingProfiles
+{
+    public class EditItemVMProfile : Profile
+    {
+        public EditItemVMProfile()
+        {
+            CreateMap<ItemModel, EditItemVM>()
+                .ForMember(d => d.ItemId, o => o.MapFrom(s => s.Id))
+                .ForMember(d => d.CollectionId, o => o.MapFrom(s => s.CollectionItems.FirstOrDefault().CollectionId))
+                .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.Resource.Url))
+                .ForMember(d => d.Fields, o => o.MapFrom(s => s.ItemOptionalFields));
+        }
+    }
+}
