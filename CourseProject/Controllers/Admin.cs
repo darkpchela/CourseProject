d4a96240dbb3ab4cb4b6973e79ab7032d464dc13
darkpchela <darkpchela2@gmail.com>
2021-01-26 00:20:07 +0300

    Extense Admin Controller

--- CourseProject/Controllers/Admin.cs
+++ CourseProject/Controllers/Admin.cs
@@ -2,6 +2,7 @@
 using BusinessLayer.Interfaces;
 using BusinessLayer.Interfaces.Authentication;
 using BusinessLayer.Interfaces.BaseCrud;
+using BusinessLayer.Models.DALModels;
 using CourseProject.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,13 +19,52 @@ namespace CourseProject.Controllers
 
         private readonly IAppUsersManager usersManager;
 
+        private readonly IThemesCrudService themesCrudService;
+
         private readonly IMapper mapper;
 
-        public Admin(ISessionHelper sessionHelper, IAppUsersManager usersManager, IMapper mapper)
+        public Admin(ISessionHelper sessionHelper, IAppUsersManager usersManager, IThemesCrudService themesCrudService, IMapper mapper)
         {
             this.sessionHelper = sessionHelper;
             this.usersManager = usersManager;
+            this.themesCrudService = themesCrudService;
             this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Themes()
+        {
+            var themes = await themesCrudService.GetAllAsync();
+            var vms = mapper.Map<IEnumerable<ThemeVM>>(themes);
+            return View(vms);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTheme(string name)
+        {
+            var theme = (await themesCrudService.GetAllAsync()).Where(t => t.Name == name);
+            if (theme is null)
+                return Json(null);
+            var model = new ThemeModel
+            {
+                Name = name
+            };
+            await themesCrudService.CreateAsync(model);
+            var vm = mapper.Map<ThemeVM>(model);
+            return Json(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteTheme(int id)
+        {
+            await themesCrudService.DeleteAsync(id);
+            return Json("OK");
         }
 
         [HttpGet]
