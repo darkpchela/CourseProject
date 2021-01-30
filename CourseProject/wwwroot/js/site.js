d94ccc216af694b6cfff8829afcd3df763049fb1
darkpchela <darkpchela2@gmail.com>
2021-01-30 21:46:03 +0300

    Add Users view spinners

--- CourseProject/wwwroot/js/site.js
+++ CourseProject/wwwroot/js/site.js
@@ -5,3 +5,48 @@
 jQuery.fn.hideV = function () {
     this.css('visibility', 'hidden');
 };
+
+const makeSearhTags = () => {
+    $('[name=tag]').click(e => {
+        e.preventDefault();
+        let value = $(e.delegateTarget).attr('data-value');
+        $('#searcher').val($('#searcher').val() + " " + value)
+        $('#searcher').change();
+    });
+};
+
+$('#searcher').change(async e => {
+    console.log($(e.target).val());
+    $('#searchSpinner').showV();
+    $('main').fadeTo(300, 0.5);
+    $('input, button').prop('disabled', true);
+    await $.ajax({
+        method: 'post',
+        url: '/Store/SearchItems/',
+        data: { text: $(e.target).val() },
+        success: (data) => {
+            $('main').empty().append(data);
+            makeSearhTags();
+        },
+        error: (error) => console.log(error)
+    });
+    $('#searchSpinner').hideV();
+    $('input, button').prop('disabled', false);
+    $('main').fadeTo(300, 1.0);
+});
+$(() => {
+    makeSearhTags();
+});
+
+$('#btnColorTheme').click(e => {
+    e.preventDefault();
+    $('html').attr('theme') == 'dark' ?
+        $('html').attr('theme', 'light') :
+        $('html').attr('theme', 'dark');
+    $.cookie('theme', $('html').attr('theme'), { expires: 186, path: '/' });
+});
+
+$(() => {
+    let theme = $.cookie('theme') ?? 'dark';
+    $('html').attr('theme', theme);
+});
