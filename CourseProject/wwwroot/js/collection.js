5985dc594e55d4c3947be6744e49714b2f737b05
darkpchela <darkpchela2@gmail.com>
2021-01-31 19:02:22 +0300

    Add removing Items deom Collection view

---
+++ CourseProject/wwwroot/js/collection.js
@@ -0,0 +1,26 @@
+﻿let itemId;
+$(() => {
+    $('[name=btnDeleteItem]').click(e => {
+        e.preventDefault();
+        itemId = $(e.delegateTarget).attr('data-id');
+        console.log(itemId)
+        let name = $(`[name=item][data-id=${itemId}]`).attr('data-name');
+        $('#delItemModal').find('#itemName').text(name);
+    });
+});
+
+$('#confirmDelItem').click(async e => {
+    e.preventDefault();
+    $('#spinner').show();
+    $('#view').fadeTo(300, 0.5);
+    $('#view a').css('pointer-events', 'none');
+    await $.post('/api/DeleteItem', { 'ItemId': itemId }, (result) => {
+        if (result.succeed === true)
+            $(`[name=item][data-id=${itemId}]`).remove()
+        else
+            console.log(result.errors)
+    });
+    $('#spinner').hide();
+    $('#view a').css('pointer-events', 'auto');
+    $('#view').fadeTo(300, 1.0);
+});
